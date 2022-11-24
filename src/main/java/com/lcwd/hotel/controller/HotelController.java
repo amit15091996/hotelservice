3bb5a0af8420196249c033defed653a53b9445dc
Amit Dewangan <amitdewangan199@gmail.com>
2022-11-24 12:32:14 +0530

    changes

--- src/main/java/com/lcwd/hotel/controller/HotelController.java
+++ src/main/java/com/lcwd/hotel/controller/HotelController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+ 
 import com.lcwd.hotel.entity.Hotel;
 import com.lcwd.hotel.service.HotelService;
 
